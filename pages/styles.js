24ad5775a67e26226dc0dc74954e19b4f68fb1bf
iuriaguiarr <iuri.aguiarr@gmail.com>
2020-11-16 21:36:56 -0300

    Error Correction 2

--- pages/styles.js
+++ pages/styles.js
@@ -18,6 +18,16 @@ export const GlobalStyle = createGlobalStyle`
         -ms-user-select: none;
             user-select: none;
   }
+  #tsparticles {
+ 
+    position: absolute;
+    left:0;
+    right:0;
+    width:100%;
+    height:100vh;
+    z-index: -1;
+
+  }
 `;
 
 export const Main = styled.main`
@@ -101,65 +111,4 @@ export const CalculatorSignal = styled(CalculatorButton)`
   &:hover {
     background-color: #1c1f21;
   }
-`;
-
-const params = {
-  particles: {
-    number: {
-      value: 160,
-      density: {
-        enable: false,
-      },
-    },
-    size: {
-      value: 3,
-      random: true,
-      anim: {
-        speed: 4,
-        size_min: 0.3,
-      },
-    },
-    line_linked: {
-      enable: false,
-    },
-    move: {
-      random: true,
-      speed: 0.5,
-      direction: "top",
-      out_mode: "out",
-    },
-  },
-  interactivity: {
-    events: {
-      onhover: {
-        enable: true,
-        mode: "bubble",
-      },
-      onclick: {
-        enable: true,
-        mode: "repulse",
-      },
-    },
-    modes: {
-      bubble: {
-        distance: 250,
-        duration: 2,
-        size: 0,
-        opacity: 0,
-      },
-      repulse: {
-        distance: 400,
-        duration: 4,
-      },
-    },
-  },
-};
-
-export const ParticlesBackground = styled(Particles).attrs(() => ({
-  params: params,
-}))`
-  position: absolute;
-  width: 100%;
-  height: 100%;
-  z-index: -1;
 `;
