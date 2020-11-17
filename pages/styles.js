595aa4bfcb03caaaa1f6f1b5afa40d92122a30df
iuriaguiarr <iuri.aguiarr@gmail.com>
2020-11-16 21:40:10 -0300

    Particles Remove

--- pages/styles.js
+++ pages/styles.js
@@ -18,16 +18,6 @@ export const GlobalStyle = createGlobalStyle`
         -ms-user-select: none;
             user-select: none;
   }
-  #tsparticles {
- 
-    position: absolute;
-    left:0;
-    right:0;
-    width:100%;
-    height:100vh;
-    z-index: -1;
-
-  }
 `;
 
 export const Main = styled.main`
